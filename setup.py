9fc4d9b3c1f649d4306b910904d53e8c46807eef
Ravipudi Sai Rajendra Prasad <rajendra@rq.st>
2019-04-24 15:05:31 +0530

    updated the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gitkanban",
-    version="0.0.2",
+    version="0.0.3",
     packages=["gitkanban"],
     url="https://github.com/deep-compute/gitkanban",
     install_requires=[
