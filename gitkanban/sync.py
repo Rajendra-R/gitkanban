dbc6b8971e90dcc181835c1bad18130f1bfcfd1d
Colin Skotarski <colinkotar@gmail.com>
2019-05-07 12:28:17 -0400

    successfully populates all tables
    Using the following command I successfully populated all the tables from models.py.
    
    gitkanban --github-access-token YOUR-TOKEN --config-file CONFIG-FILE.json sync --db sqlite:///tmp.db

--- gitkanban/sync.py
+++ gitkanban/sync.py
@@ -43,12 +43,14 @@ class SyncCommand:
             # add all Issue rows from repo
             self.populate_issues(repo, repository_row)
 
+        # commit any changes in transaction buffer
+        self.session.commit()
+
     def populate_issues(self, repo, repo_row):
         # get open issues from repo
         open_issues = repo.get_issues(state='open')
 
         for issue in open_issues:
-
             # get closed user and add to table
             closed_by = issue.closed_by
             self.add_and_get_user(closed_by)
@@ -60,13 +62,19 @@ class SyncCommand:
                               repository=repo_row)
             self.session.add(issue_row)
 
+            # append issues to users and issues to labels
+            self.append_issues_users(issue_row, issue.assignees)
+            self.append_issues_labels(issue_row, issue.labels)
+
             # add all IssueComment rows from issue
             self.populate_issue_comments(issue, issue_row)
+
+        # commit any changes in transaction buffer
+        self.session.commit()
 
     def populate_issue_comments(self, issue, issue_row):
         # for each issue get issue comments
         for comment in issue.get_comments():
-
             # add and get IssueComment's User
             user_row = self.add_and_get_user(comment.user)
 
@@ -74,6 +82,9 @@ class SyncCommand:
             issue_comment_row = IssueComment(issue=issue_row, user=user_row,
                                              body=comment.body)
             self.session.add(issue_comment_row)
+
+        # commit any changes in transaction buffer
+        self.session.commit()
 
     # add Label row to session by creating from config file
     def populate_labels(self):
@@ -88,6 +99,9 @@ class SyncCommand:
 
             label_row = Label(name=name, description=description, color=color)
             self.session.add(label_row)
+
+        # commit any changes in transaction buffer
+        self.session.commit()
 
     # add Organization row to session by creating or find existing
     #   :return: Organization row
@@ -109,9 +123,12 @@ class SyncCommand:
                                    description=org_git.description, email=org_git.email)
             self.session.add(org_row)
 
+        # commit any changes in transaction buffer
+        self.session.commit()
+
         return org_row
 
-    # add User row to session if it doesn't exist
+    # add User row to session if it doesn't exist, return User
     def add_and_get_user(self, user):
 
         user_row = None
@@ -130,7 +147,31 @@ class SyncCommand:
                             email=user.email, avatar_url=user.avatar_url)
             self.session.add(user_row)
 
+        # commit any changes in transaction buffer
+        self.session.commit()
+
         return user_row
+
+    # add Label row to session if it doesn't exist, return Label
+    def add_and_get_label(self, label):
+
+        label_row = None
+
+        # if there is no Label, return empty Label
+        if not label:
+            return Label(name=None, description=None, color=None)
+
+        # if existing Label
+        if self.session.query(Label).filter_by(name=label.name).first():
+            label_row = self.session.query(Label).filter_by(name=label.name).first()
+        else:  # if Label does not exist, create and add
+            label_row = Label(name=label.name, description=label.description, color=label.color)
+            self.session.add(label_row)
+
+        # commit any changes in transaction buffer
+        self.session.commit()
+
+        return label_row
 
     # gets all repositories from config file
     #   :return: list of Repository objects
@@ -143,24 +184,38 @@ class SyncCommand:
         # get all repos from each repo_group
         return [git.get_repo(str(*i.values())) for k, v in repo_groups.items() for i in v]
 
-    def append_issues_users(self):
-        pass
+    def append_issues_users(self, issue_row, users):
 
-    def append_issues_labels(self):
-        pass
+        for user in users:
+            # create User
+            user_row = self.add_and_get_user(user)
+            # append issue to user
+            issue_row.assignees.append(user_row)
+
+        # commit any changes in transaction buffer
+        self.session.commit()
+
+    def append_issues_labels(self, issue_row, labels):
+        for label in labels:
+            # create Label
+            label_row = self.add_and_get_label(label)
+            # append issue to label
+            issue_row.labels.append(label_row)
+
+        # commit any changes in transaction buffer
+        self.session.commit()
 
     # populates each table manually to test relaionships, etc.
     def populate_all(self):
 
-        # populate Repository rows
-        self.populate_repos()
-
         # populate all labels from config file
         self.populate_labels()
 
-        # append issues to users and issues to labels
-        self.append_issues_users()
-        self.append_issues_labels()
+        # populate Repository,
+        self.populate_repos()
+
+        # commit any changes in transaction buffer
+        self.session.commit()
 
     def run(self):
         self.config_json = self.gitkanban.config_json
@@ -180,4 +235,3 @@ class SyncCommand:
 
         # populate all tables
         self.populate_all()
-        self.session.commit()
